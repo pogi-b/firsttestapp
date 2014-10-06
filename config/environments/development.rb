030092d4d22767adc1b35cb57ddad873cf9a3069
pogi-b <pogib.dev@gmail.com>
2014-10-06 23:46:39 +0200

    blablablabla

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #require 'byebug'
+  #Byebug.wait_connection = true
+  #Byebug.start_server('localhost', 8001)
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
